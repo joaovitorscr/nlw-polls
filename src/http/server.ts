a394595daf8280647a3633694b6e14940385b111
joaovitorscr <98563963+joaovitorscr@users.noreply.github.com>
2024-02-10 22:13:44 -0300

    FEAT: Redis ranking and websocket to show results in real time

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,16 +3,22 @@ import { createPoll } from './routes/create-poll'
 import { GetPoll } from './routes/get-poll'
 import { VoteOnPoll } from './routes/vote-on-poll'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
+
 app.register(cookie, {
   secret: 'polls-app-nlw',
   hook: 'onRequest',
   parseOptions: {},
 })
+app.register(websocket)
+
 app.register(createPoll)
 app.register(GetPoll)
 app.register(VoteOnPoll)
+app.register(pollResults)
 
 app
   .listen({
